c658afde112571b77c8a1205d5af6095c6cf9216
Wesley Sun <wsun@lcs.on.ca>
2019-10-02 11:59:59 -0400

    Entered speech "I am thinking of a number between 1 and 100

--- AGuessingGame/ViewController.swift
+++ AGuessingGame/ViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    // MARK: Properties
+    
+    // MARK: Initializers        // Do any additional setup after loading the view.
+    
+    // MARK: Methods (functions)- behaviours
+    
+    // Runs as soon as the view becomes visible to the user
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        // Make an object named "synthesizer" , which is an instance of the
+        let synthesizer = AVspeechsynthesizer()
+        
+        
+        //Make a string that contains what we want the computer to say
+        let message = "I'm thinking of a number between 1 and 100. Guess what it is."
+        
+        
+        //Make an object named 'utterence' , which is an instance of the class
+        // Avspeechutterence
+        var utterence = AVspeechutterence (string: message)
+        
+        //Speak the Message
+        synthesizer.speak (utterence)
     }
-
-
+    
+    
 }
+
+
+
+
+
+
+
 
